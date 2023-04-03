6bb55e41995e57154a612bbd71b9f653d18659a7
spacemc2 <mtorresroadby@gmail.com>
2023-04-03 15:35:29 -0400

    project

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,48 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  return {
+    store: {
+      todoListItems: ["Buy milk", "Buy eggs", "Buy bread", "Buy butter"],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      // exampleFunction: () => {
+      // 	getActions().changeColor(0, "green");
+      // },
+      //   loadSomeData: () => {
+      //     /**
+      // 				fetch().then().then(data => setStore({ "foo": data.bar }))
+      // 			*/
+      //   },
+      // changeColor: (index, color) => {
+      //   //get the store
+      //   const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      //   //we have to loop the entire demo array to look for the respective index
+      //   //and change its color
+      //   const demo = store.demo.map((elm, i) => {
+      //     if (i === index) elm.background = color;
+      //     return elm;
+      //   });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+      //   //reset the global store
+      //   setStore({ demo: demo });
+      // },
+
+      deleteTodoListItem: (index) => {
+        const store = getStore();
+        const todoListItems = store.todoListItems.filter(
+          (item, i) => i !== index
+        );
+        setStore({ todoListItems: todoListItems });
+      },
+
+      addTodoListItem: (item) => {
+        const store = getStore();
+        const todoListItems = store.todoListItems.concat(item);
+        setStore({ todoListItems: todoListItems });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,44 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hola Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="text-center mt-5">
+    <p>This project has been created by:</p>
+    <ul className=" list-group">
+      <li>
+        <a
+          className="link-secondary text-decoration-none"
+          href="https://github.com/Balim1"
+        >
+          Balim1
+        </a>
+      </li>
+      <li>
+        <a
+          className="link-secondary text-decoration-none"
+          href="https://github.com/joaarg86"
+        >
+          joaarg86
+        </a>
+      </li>
+      <li>
+        <a
+          className="link-secondary text-decoration-none"
+          href="https://github.com/raulyfs11"
+        >
+          raulyfs11
+        </a>
+      </li>
+      <li>
+        <a
+          className="link-secondary text-decoration-none"
+          href="https://github.com/spacemc2"
+        >
+          spacemc2
+        </a>
+      </li>
+    </ul>
+  </div>
 );
